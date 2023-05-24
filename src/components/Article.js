4d07e638a26ea8944b0b5889a4e078852bc63c93
boredrum <vvmaiboroda@gmail.com>
2023-05-24 10:12:38 +0300

    init commit

---
+++ src/components/Article.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ArticleBody from "./ArticleBody";
+import ArticleActions from "./ArticleActions";
+import ArticleAuthor from "./ArticleAuthor";
+
+function Article() {
+    return (
+    <div className="article__title">
+        <h2>NVIDIA Accelerated AI on Azure</h2>
+      <ArticleBody />
+      <ArticleActions />
+      <ArticleAuthor />
+    </div> 
+    );
+  }
+  export default Article;
---
+++ src/components/ArticleActions.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ArticleButtons() {
+    return (
+    <div className="article__buttons">
+        <button>Read</button>
+        <button>Mark as read</button>
+        <button>Mark as unread</button>
+    </div> 
+    );
+  }
+  export default ArticleButtons;
